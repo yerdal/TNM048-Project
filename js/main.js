302fa664349a328ae6799e86e5c739bcd2dc0316
yerdal <yusuf.erdal.93@gmail.com>
2016-03-01 13:37:47 +0100

    plot

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ d3.csv("data/Swedish_Election_2014.csv", function (data)
 
      plot1 = new plot();
 
-     map1 = new map(data);
+     //map1 = new map(data);
 
      //plot1 = new plot();
 
-     bc = new barchart(data);
+     //bc = new barchart(data);
 
 });
--- js/plot.js
+++ js/plot.js
@@ -29,36 +29,18 @@ function plot() {
     height = plotDiv.height() - margin.top - margin.bottom;
   //console.log("plotheight", plotDiv.height());
   var x = d3.scale.linear()
-    .range([0, width]);
+    .range([0, width]).domain([2002,2014]);
 
 
   var y = d3.scale.linear()
-    .range([height, 0]);
-
+    .range([height, 0])
+    .domain([0, 60]);
 
   var svg = d3.select("#plotchart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-  var g = svg.append("svg:g")
-    .attr("transform", "translate(0, 200)");
-
-  var line = d3.svg.line()
-    .x(function(d, i) {
-      console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-      // return the X coordinate where we want to plot this datapoint
-      return x(i);
-    })
-    .y(function(d) {
-      // verbose logging to show what's actually being done
-      console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-      // return the Y coordinate where we want to plot this datapoint
-      return y(d);
-    })
-
 
   var xAxis = d3.svg.axis()
     .scale(x)
@@ -72,16 +54,19 @@ function plot() {
 
   var color = d3.scale.category20();
 
+  var line = d3.svg.line()
+      .x(function(d) { console.log("year", d.year);
+        return x(d.year); })
+      .y(function(d) { //console.log("vote", d);
+        return y(d.votes); })
+      .interpolate("linear");
 
   //load data for 2014
   d3.csv("data/Swedish_Election_2014.csv", function(error, data) {
     self.data = data;
     data.forEach(function(d) {
       data2014.push(d);
-      //votes2014.push(d.votes);
     })
-
-
   });
 
   //load data for 2010
@@ -89,8 +74,6 @@ function plot() {
     self.data = data;
     data.forEach(function(d) {
       data2010.push(d);
-      //console.log("Vote", d.votes);
-      //votes2010.push(d.votes);
     })
   });
 
@@ -98,9 +81,7 @@ function plot() {
   d3.csv("data/Swedish_Election_2006.csv", function(error, data) {
     self.data = data;
     data.forEach(function(d) {
-      //console.log( "HEJ", d);
       data2006.push(d);
-      //votes2006.push(d.votes);
     })
   });
 
@@ -108,74 +89,67 @@ function plot() {
   d3.csv("data/Swedish_Election_2002.csv", function(error, data) {
     self.data = data;
     data.forEach(function(d) {
-      //console.log( "HEJ", d);
       data2002.push(d);
-      //votes2002.push(d.votes);
-
     })
-
-
     //define the domain of the scatter plot axes
-    x.domain(d3.extent(xValues, function(d) {
-      return d;
-    }));
-    y.domain([0, d3.max(yValues, function(d) {
-      return d;
-    })]);
     draw();
   });
 
+
   function draw() {
-    /*console.log("data2014", data2014[0]);
-    console.log("data2010", data2010[0]);
-    console.log("data2006", data2006[0]);
 
-    console.log("data2002", data2002[0]);
-    console.log("Votes2014", votes2014[0]);
-    console.log("Votes2010", votes2010[0]);
-    console.log("Votes2006", votes2006[0]);
-    console.log("Votes2002", votes2002[0]);*/
-    //electionForParty(data2014);
-    console.log(data2006);
-
+    //Filter data for every year
     var votes2014 = calcNationalResults(data2014);
     var votes2010 = calcNationalResults(data2010);
     var votes2006 = calcNationalResults(data2006);
     var votes2002 = calcNationalResults(data2002);
 
+    //Create array for every party with the result in every election from 2002-2014
+    // {2014, 2010, 2006, 2002}
     var moderatVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Moderaterna");
     var centerVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Centerpartiet");
     var folkVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Folkpartiet");
     var kristVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Kristdemokraterna");
     var milVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Miljöpartiet");
+    var socVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Socialdemokraterna");
+    var vansterVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Vänsterpartiet");
+    var sdVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "Sverigedemokraterna");
+    var ovrigVotes = forEachParty(votes2014, votes2010, votes2006, votes2002, "övriga partier");
 
-    /*for(var i = 0; i <= votes2014.length; i++){
-      console.log("LOG", votes2014[i].party);
-      if(votes2014[i].party == "Moderaterna"){
-        moderatVotes.push(votes2014[i]);
-      }
-    }
-    console.log("ModeratVotes", moderatVotes);*/
+    var totData = [moderatVotes, centerVotes];
 
-    /*    var graph = d3.select("#graph").append("svg:svg")
-    			      .attr("width", w + m[1] + m[3])
-    			      .attr("height", h + m[0] + m[2])
-    			    .append("svg:g")
-    			      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");*/
+    /*var dataGroup = d3.nest()
+      .key(function(d){
+        console.log("test", d.party);
+        return d.party;
+      })
+      .entries(votes2014);
+
+      console.log("grupp", dataGroup);*/
+
+    /*totData.push(moderatVotes);
+    totData.push(centerVotes);
+    totData.push(folkVotes);
+    totData.push(kristVotes);
+    totData.push(milVotes);
+    totData.push(socVotes);
+    totData.push(vansterVotes);
+    totData.push(sdVotes);
+    totData.push(ovrigVotes);*/
+    //console.log(moderatVotes);
+    //console.log("tot", totData);
 
     // Add x axis and title.
     svg.append("g")
-      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
       .append("text")
       .attr("class", "label")
       .attr("x", width - 50)
       .attr("y", -6)
-      .data(self.data)
       .text("Election Year");
 
-    // Add y axis and title.
+
     svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -185,37 +159,54 @@ function plot() {
       .attr("y", 8)
       .attr("x", -100)
       .attr("dy", ".71em")
-      .data(self.data)
       .text("Election result (%)");
+
+
+      svg.append("svg:path")
+      .attr("d", line(moderatVotes))
+      //.data(moderatVotes)
+      .attr("class", "line")
+      /*.attr("stroke", function(d){
+          console.log("tet", d.votes);
+            return d.votes;
+      })*/
+      .attr("stroke-width", 3);
 
   }
 
   function forEachParty(data1, data2, data3, data4, chosenParty) {
     var result = [];
+
     data1.forEach(function(d) {
       if (d.party == chosenParty) {
-        result.push(d);
+        result.push({"year": 2014, 
+                      "party": d.party,
+                      "votes": d.votes});
       }
     })
-
     data2.forEach(function(d) {
       if (d.party == chosenParty) {
-        result.push(d);
+        result.push({"year": 2010, 
+                      "party": d.party,
+                      "votes": d.votes});
       }
     })
 
     data3.forEach(function(d) {
       if (d.party == chosenParty) {
-        result.push(d);
+        result.push({"year": 2006, 
+                      "party": d.party,
+                      "votes": d.votes});
       }
     })
-
     data4.forEach(function(d) {
       if (d.party == chosenParty) {
-        result.push(d);
+        result.push({"year": 2002, 
+                      "party": d.party,
+                      "votes": d.votes});
       }
     })
-    console.log(result);
+    //console.log(result);
     return result;
   }
 
@@ -227,7 +218,6 @@ function plot() {
       }
     }
 
-
     var NUM_PARTIES = 9;
     var nationalResults = [];
     var parties = [];
@@ -235,7 +225,7 @@ function plot() {
     var vote = 0;
     for (var i = 0; i < NUM_PARTIES; i++) {
       nationalResults.push({
-        "party": data[i].party,
+        "party": filteredData[i].party,
         "region": "Sweden",
         "votes": 0
       })
